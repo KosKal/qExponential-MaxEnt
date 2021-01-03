dd5f73c30813ed7b39ff57ce4827265be1c752fb
Konstantinos Kaloudis <koskal89@gmail.com>
2021-01-03 16:41:01 +0200

    Update toolbox.jl

--- toolbox.jl
+++ toolbox.jl
@@ -16,8 +16,41 @@ function dqexp(xs,q,b)
 end
 
 ###########################################################
+# Monte Carlo repetitions of the qExponential \beta-parameter and MSE calculation (Figure 3)
+# Estimation methods: Shannon - Renyi (linear constraints) - Renyi (escort constraints)
+# ss: sample size, nreps: number of Monte Carlo repetitions, q,b: parameters of the qExponential
 
-# Monte Carlo repetitions of the qExponential \beta-parameter and MSE calculation
+function monteCarlo_betas(ss, nreps, q, b)
+   qhat = zeros(nreps)
+   bhat_sh = zeros(nreps)
+   bhat_r1 = zeros(nreps)
+   bhat_r2 = zeros(nreps)
+   betas = zeros(nreps,3)
+   for i in 1:nreps
+      global data
+      # generate qexp sample of size ss
+      data = rqexp(ss,q,b)
+      ##################################################################
+      g(x) = 1/length(data)*sum(data ./ (1 .+ data ./ x)) * (1. + (length(data) / sum(log.(1 .+ data / x))) ) - x
+      strue = 1/(b * (1-q))
+      sigmahat = find_zero(g, rand(Uniform(strue-ee,strue+ee)))
+      # calculate estimators
+      thetahat = length(data)/sum(log.(1 .+ data / sigmahat))
+      qhat[i] = thetahat / (thetahat + 1.)
+      ##
+      bhat_sh[i] = 1. / (sigmahat * (1. - qhat[i]))
+      bhat_r1[i] = 1/((2q-1)*mean(data)) #linear constraint estimator
+      bhat_r2[i] = 1/(q*mean(data)) #escort constraint estimator
+   end
+   betas[:,1] = bhat_sh
+   betas[:,2] = bhat_r1
+   betas[:,3] = bhat_r2
+   return(betas)
+end
+
+###########################################################
+
+# Monte Carlo repetitions of the qExponential \beta-parameter and MSE calculation (Figure 5)
 # Estimation methods: Shannon - Renyi (linear constraints) - Renyi (escort constraints)
 # ss: sample size, nreps: number of Monte Carlo repetitions, q,b: parameters of the qExponential
 function monteCarloMSE(ss, nreps, q, b)
